ebb39fe009db775d9593a63df26c48aa348c3f06
dixu11 <daniel.szlicht25@gmail.com>
2018-10-20 15:01:15 +0200

    trelemorele na masterze

--- src/Majn.java
+++ src/Majn.java
@@ -2,5 +2,6 @@ public class Majn {
     public static void main(String[] args) {
         System.out.println("pierfsza wersja kodu");
         System.out.println("ulepszyliśmy nasz kod panie kapitanie");
+        System.out.println("Zmieniam maina na masterze");
     }
 }
