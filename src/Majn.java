74ab49c887c28524f1f2e658af40ec1ce5b539aa
dixu11 <daniel.szlicht25@gmail.com>
2018-10-20 13:52:01 +0200

    pora na pierwszy update

--- src/Majn.java
+++ src/Majn.java
@@ -1,5 +1,6 @@
 public class Majn {
     public static void main(String[] args) {
         System.out.println("pierfsza wersja kodu");
+        System.out.println("ulepszyliśmy nasz kod panie kapitanie");
     }
 }
