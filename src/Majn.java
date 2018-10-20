f8e1b19a0826340920d859a5e8789274a8107f0e
dixu11 <daniel.szlicht25@gmail.com>
2018-10-20 15:27:01 +0200

    jakiś komit

--- src/Majn.java
+++ src/Majn.java
@@ -2,5 +2,6 @@ public class Majn {
     public static void main(String[] args) {
         System.out.println("pierfsza wersja kodu");
         System.out.println("ulepszyliśmy nasz kod panie kapitanie");
+        System.out.println("Tu jest nowa piękna zmiana w kodzie mym");
     }
 }
