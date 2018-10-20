8c6274a5d0f3f75ce3f603364600dcafad7dc5ac
dixu11 <daniel.szlicht25@gmail.com>
2018-10-20 14:51:53 +0200

    Melduje dodanie klasy user, sir

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+    private String lastName;
+    private int age;
+
+    public User(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
